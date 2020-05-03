ae9d03b492c6392236aaef831c7893e4b53c996f
Victor Porton <porton@narod.ru>
2020-05-03 04:43:38 +0300

    data algorithms

--- assembly/model.ts
+++ assembly/model.ts
@@ -1,26 +1,34 @@
 import {
     PersistentMap,
-    PersistentVector,
+    PersistentSet,
 } from "near-sdk-as";
+
+// FIXME: We assume that the Earth is square. People living near Earth zero meridian can't find all peers.
 
 class Quadrant {
     degree: number;
     x: number;
     y: number;
+    constructor(degree: number, x: number, y: number) {
+        this.degree = degree;
+        this.x = x;
+        this.y = y;
+    }
     superQuadrant(): Quadrant | null {
         if(this.degree == 0) return null;
-        let result: Quadrant = new Quadrant();
-        result.degree = this.degree - 1;
-        result.x = Math.floor(this.x / 2);
-        result.y = Math.floor(this.y / 2);
-        return result;
+        return new Quadrant(this.degree - 1, Math.floor(this.x / 2), Math.floor(this.y / 2));
     }
 }
 
+const MAX_DEGREE = 20;
+
 class Person {
     fullname: string;
-    lattitude: number;
+    latitude: number;
     longtitude: number;
+    setCoords(latitude: number, longtitude: number) {
+        
+    }
 }
 
 @nearBindgen
@@ -30,4 +38,33 @@ export class TextMessage {
     }
 }
 
-export const personsMap = new PersistentMap<Quadrant, Person[]>("m");
+export const personsMap = new PersistentMap<Quadrant, PersistentSet<Person>>("m");
+
+function persistentCollectionForQuadrant(quadrant: Quadrant) : PersistentSet<Person> {
+    return new PersistentSet("v"+quadrant.degree+'/'+quadrant.x+'/'+quadrant.y);
+}
+
+// TODO: Duplicate code with the below.
+function addPerson(person: Person): void {
+    for(let degree = MAX_DEGREE; degree >= 0; --degree) {
+        const k = 2**degree;
+        let x = Math.floor(((person.latitude+90) / 180) * k);
+        let y = Math.floor(((person.longtitude+180) / 360) * k);
+        let quadrant = new Quadrant(degree, x, y);
+        let set = persistentCollectionForQuadrant(quadrant);
+        personsMap.set(quadrant, set);
+        set.add(person);
+    }
+}
+
+// TODO: Duplicate code with the above.
+function removePerson(person: Person): void {
+    for(let degree = MAX_DEGREE; degree >= 0; --degree) {
+        const k = 2**degree;
+        let x = Math.floor(((person.latitude+90) / 180) * k);
+        let y = Math.floor(((person.longtitude+180) / 360) * k);
+        let quadrant = new Quadrant(degree, x, y);
+        let set = persistentCollectionForQuadrant(quadrant);
+        set.delete(person);
+    }
+}
--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -50166,7 +50166,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38019" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46681" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
